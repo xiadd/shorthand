7a86f0ba1d83fdd96b1e75dcff4fd4259d94c213
xiadd <1278609551@qq.com>
2016-07-03 00:10:40 +0800

    图灵机器人实例

---
+++ app/libs/turingRobot.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const config = require('../../config');
+
+function getTuringResponse(info) {
+  if(typeof info !== 'string') {
+    info = info.toString();
+  }
+  var options = {
+    method:'GET',
+    url: 'http://apis.baidu.com/turing/turing/turing?key=879a6cb3afb84dbf4fc84a1df2ab7319&info='+info,
+    headers: {
+      'apikey': config.turingKey
+    }
+  };
+  return new Promise((resolve, reject) => {
+    request(options, function (err, res, body) {
+      console.log(body)
+      if (res) {
+        resolve(body);
+      } else {
+        reject(err);
+      }
+    });
+  })
+}
+
+module.exports = getTuringResponse;
