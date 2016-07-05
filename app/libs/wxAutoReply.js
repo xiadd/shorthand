7149474f237a0a437d27d50899e8f974bdac09fc
xiadd <1278609551@qq.com>
2016-07-05 10:10:19 +0800

    修改自动回复错误

--- app/libs/wxAutoReply.js
+++ app/libs/wxAutoReply.js
@@ -8,8 +8,8 @@ const request = require('request');
  * @returns {string} 返回xml字符串用作消息内容
  */
 
-function autoReply(requestData, info) {
-  switch (requestData.msgtype) {
+function autoReply(msgType, requestData, info) {
+  switch (msgType) {
     case 'text':
       var resMsg = '<xml>' +
         '<ToUserName><![CDATA[' + requestData.fromusername + ']]></ToUserName>' +
@@ -19,6 +19,15 @@ function autoReply(requestData, info) {
         '<Content><![CDATA['+info+']]></Content>' +
         '</xml>';
       break;
+    //关注事件
+    case 'subscribe':
+      var resMsg = '<xml>' +
+        '<ToUserName><![CDATA[' + requestData.fromusername + ']]></ToUserName>' +
+        '<FromUserName><![CDATA[' + requestData.tousername + ']]></FromUserName>' +
+        '<CreateTime>' + parseInt(new Date().valueOf() / 1000) + '</CreateTime>' +
+        '<MsgType><![CDATA[text]]></MsgType>' +
+        '<Content><![CDATA['+info+']]></Content>' +
+        '</xml>';
   }
 
   return resMsg;
