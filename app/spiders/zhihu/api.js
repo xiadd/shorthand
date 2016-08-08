e44eb13a86f807762870f7be01ba2ad46856f47e
xiadd <1278609551@qq.com>
2016-08-08 23:27:21 +0800

    知乎爬虫

--- app/spiders/zhihu/api.js
+++ app/spiders/zhihu/api.js
@@ -2,5 +2,5 @@
  * Created by xiadd on 7/14/16.
  */
 module.exports = {
-  topicUrl: 'https://www.zhihu.com/topics'
+  followees: 'https://www.zhihu.com/node/ProfileFolloweesListV2' //获取粉丝列表
 };
--- app/spiders/zhihu/zhihu.js
+++ app/spiders/zhihu/zhihu.js
@@ -2,67 +2,67 @@
 /**
  * Created by xiadd on 7/14/16.
  */
+
+var AV = require('leanengine');
 const cheerio = require('cheerio');
 const config = require('../../../config');
 const Promise = require('bluebird');
 const request = Promise.promisifyAll(require('request'));
 const api = require('./api');
 
-function getAllTopicsHref() {
-  let topicsArr = [];
-  return request.getAsync({
-    url: api.topicUrl
-  }).then(res => {
-    let $ = cheerio.load(res.body);
-    let topics = $('.zm-topic-cat-item>a');
-    topics.each((i, v) => {
-      topicsArr.push(api.topicUrl + v.attribs.href);
-    });
-    return topicsArr;
-  })
-}
 
-function getChildrenTopics() {
-  let childrenTopics = {};
-  getAllTopicsHref().then(data => {
-    console.log(data)
-    data.forEach(v => {
-      let topicHref = v;
-      request({
-        method: 'get',
-        url: topicHref
-      })
-    });
-    console.log(childrenTopics)
-  })
-}
+AV.init({
+  appId: 'C2yCiRi9yETYr8RvEJvs276d-gzGzoHsz',
+  appKey: 'O4zRMgWdUMj6H3PJQMJ7RHpg',
+  masterKey: '9Kc9CjYKKr3EQdRX3buxSRC5'
+});
+//数据库
+var Follwees = AV.Object.extend('Follees');
 
-function extractContent(content) {
-  if (typeof content !== 'string') {
-    content = content.toString();
-  }
-  let $ = cheerio.load(content);
-  return $('a.question_link').text();
-}
-
-function getZhihuContent() {
-  let options = {
-    url: 'https://www.zhihu.com/topic/19550517/top-answers',
-    method: 'get',
-    qs: {
-      page: 12
-    },
+function getFollowees() {
+  var options = {
+    url: api.followees,
+    method: 'post',
     headers: {
-      'Cookie': config.zhihuCookie
+      'Cookie': config.zhihuCookie,
+      'X-Xsrftoken': config.zhihuXsrfToken
+    },
+    form: {
+      'method': 'next',
+      'params': '{"offset":40,"order_by":"created","hash_id":"8e54246e804fef8aa43434190f1c1870"}'
     }
   };
-  request(options, function (err, res, body) {
-    let data = extractContent(body).split('\n');
-    data = data.filter(v => {
-      return v.length > 0;
-    });
-    console.log(data)
+
+  return new Promise(function (resolve, reject) {
+    request(options, function (err, res, body) {
+      if(typeof JSON.parse(body) === 'object') {
+        resolve(JSON.parse(body));
+      } else {
+        reject(new TypeError('返回数据格式不正确').message);
+      }
+    })
   })
 }
 
-module.exports = getChildrenTopics;
+function saveResults() {
+  if(typeof getFollowees !== 'function'){
+    throw new TypeError('传入正确的函数');
+  }
+  getFollowees().then(function (data) {
+    if(!data['msg']){
+      return new Error('数据格式不正确');
+    }
+    var resData = data['msg'];
+    resData.forEach(function (item) {
+      var newFollee = new Follwees();
+      newFollee.set('info', item);
+      newFollee.save().then(function () {
+        console.log('success');
+      }, function (err) {
+        console.log(err);
+      })
+    });
+  });
+}
+
+module.exports = saveResults;
